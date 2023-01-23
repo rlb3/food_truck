4c999bcdf7fd865d883402546d1fcfa0a1ef5951
Robert Boone <robert@rlb3.com>
2023-01-22 23:24:19 -0600

    Add seed data to the database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,8 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+require Logger
+
 headers =
   ~w[locationid Applicant FacilityType cnn LocationDescription Address blocklot block lot permit Status FoodItems X Y Latitude Longitude Schedule dayshours NOISent Approved Received PriorPermit ExpirationDate Location "Fire Prevention" Districts "Police Districts" "Supervisor Districts" "Zip Codes" "Neighborhoods (old)"
 ]
@@ -17,10 +19,24 @@ headers =
 
 NimbleCSV.define(FoodTruckParser, separator: ",", escape: "\"")
 
-data =
+trucks =
   Path.join([__DIR__, "Mobile_Food_Facility_Permit.csv"])
   |> File.stream!(read_ahead: 500)
   |> FoodTruckParser.parse_stream(skip_headers: true)
   |> Enum.into([])
   |> Enum.map(fn line -> Enum.zip(headers, line) end)
   |> Enum.map(fn line -> Enum.into(line, %{}) end)
+
+for truck <- trucks do
+  FoodTruck.Repo.insert!(%FoodTruck.Trucks{
+    applicant: truck["Applicant"],
+    facility_type: truck["FacilityType"],
+    location_description: truck["LocationDescription"],
+    address: truck["Address"],
+    status: truck["Status"],
+    food_items: truck["FoodItems"],
+    schedule: truck["Schedule"]
+  })
+end
+
+Logger.info("Database seeded!")
