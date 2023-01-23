0f92c52308361308956fb86ac769a18dafb54ef2
Robert Boone <robert@rlb3.com>
2023-01-22 23:06:33 -0600

    Parse CSV file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+headers =
+  ~w[locationid Applicant FacilityType cnn LocationDescription Address blocklot block lot permit Status FoodItems X Y Latitude Longitude Schedule dayshours NOISent Approved Received PriorPermit ExpirationDate Location "Fire Prevention" Districts "Police Districts" "Supervisor Districts" "Zip Codes" "Neighborhoods (old)"
+]
+  |> Enum.map(fn header -> String.replace(header, "\"", "") end)
+
+NimbleCSV.define(FoodTruckParser, separator: ",", escape: "\"")
+
+data =
+  Path.join([__DIR__, "Mobile_Food_Facility_Permit.csv"])
+  |> File.stream!(read_ahead: 500)
+  |> FoodTruckParser.parse_stream(skip_headers: true)
+  |> Enum.into([])
+  |> Enum.map(fn line -> Enum.zip(headers, line) end)
+  |> Enum.map(fn line -> Enum.into(line, %{}) end)
